e101d3da5d8ff61c8f95a886ef7fe5b41ec7aed6
samwhelp <samwhelp@users.noreply.github.com>
2019-03-11 18:16:34 +0800

    tool/prototype/download-eliu-gcin-source/php-dom-xpath-file-get-contents

---
+++ ubuntu/18.04/tool/prototype/download-eliu-gcin-source/php-dom-xpath-file-get-contents/download.php
@@ -0,0 +1,46 @@
+#!/usr/bin/env php
+<?php
+
+	// http://php.net/manual/en/function.error-reporting.php
+	error_reporting(E_ALL & ~(E_WARNING|E_NOTICE));
+
+	$base_url = 'https://hyperrate.com/gcin-source';
+
+	$doc = new DOMDocument; // http://php.net/manual/en/class.domdocument.php
+
+	$doc->preserveWhiteSpace = false; // http://php.net/manual/en/class.domdocument.php#domdocument.props.preservewhitespace
+
+	$doc->loadHTMLFile($base_url); // http://php.net/manual/en/domdocument.loadhtmlfile.php
+
+	$xpath = new DOMXPath($doc); // http://php.net/manual/en/class.domxpath.php
+
+	$query = '//a/@href';
+
+	$nodes = $xpath->query($query); // http://php.net/manual/en/domxpath.query.php
+
+	if (is_null($nodes)) { // http://php.net/manual/en/function.is-null.php
+		return;
+	}
+
+
+	$list = '';
+	// http://php.net/manual/en/class.domnodelist.php
+	// http://php.net/manual/en/class.domnode.php
+	foreach ($nodes as $node) {
+
+		$source_file_name = $node->nodeValue;
+
+		if (!strstr($source_file_name, 'gcin')) { // http://php.net/manual/en/function.strstr.php
+			continue;
+		}
+
+		$source_url = $base_url . '/' . $source_file_name;
+
+		echo 'Download: ' . $source_url . PHP_EOL;
+
+		$file_contents = file_get_contents($source_url); // http://php.net/manual/en/function.file-get-contents.php
+
+		$save_file_path = __DIR__ . '/' . $source_file_name;
+
+		file_put_contents($save_file_path, $file_contents); // http://php.net/manual/en/function.file-put-contents.php
+	}
